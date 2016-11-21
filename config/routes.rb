ea738b6aefdf26fc829014f42883a5c5b0c72d56
Raghu Betina <raghu@firstdraft.com>
2016-11-21 04:55:29 +0000

    Generate organizations resource with starter:resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
+  # Routes for the Organization resource:
+  # CREATE
+  get "/organizations/new", :controller => "organizations", :action => "new"
+  post "/create_organization", :controller => "organizations", :action => "create"
+
+  # READ
+  get "/organizations", :controller => "organizations", :action => "index"
+  get "/organizations/:id", :controller => "organizations", :action => "show"
+
+  # UPDATE
+  get "/organizations/:id/edit", :controller => "organizations", :action => "edit"
+  post "/update_organization/:id", :controller => "organizations", :action => "update"
+
+  # DELETE
+  get "/delete_organization/:id", :controller => "organizations", :action => "destroy"
+  #------------------------------
+
   devise_for :users
   # Routes for the User resource:
   # READ
