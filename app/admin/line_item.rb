53702c2112903c8b237a6d338f14f5bdb0230d7d
Raghu Betina <raghu@firstdraft.com>
2016-11-21 04:56:30 +0000

    Whitelist columns in LineItem for ActiveAdmin

--- app/admin/line_item.rb
+++ app/admin/line_item.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register LineItem do
 
+ permit_params :inventory_id, :proposed_trade_id, :quantity
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
